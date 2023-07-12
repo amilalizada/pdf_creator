028f01e7077f753e30a480a69da6acc90aee48b3
amilalizada <elizade_amil@inbox.ru>
2023-07-12 17:18:38 +0400

    in progress

--- core/database.py
+++ core/database.py
@@ -18,7 +18,7 @@ db_connection = StrictMySQLDatabase(
     user=settings.db_user,
     password=settings.db_password,
     port=3309,
-    host="127.0.0.1",
+    host="localhost",
 )
 
 class BaseModel(Model):
