f3fc1c6bf06c655d98d63b35dd4bb47d109a3e78
amilalizada <elizade_amil@inbox.ru>
2023-08-28 12:34:20 +0400

    test folders

--- app/tasks.py
+++ app/tasks.py
@@ -4,4 +4,5 @@ import shutil
 
 @repeat_every(seconds=21600, raise_exceptions=True)
 def remove_files() -> None:
-    ...
+    shutil.rmtree("invoices/", ignore_errors=True)
+    shutil.rmtree("ttas/", ignore_errors=True)
